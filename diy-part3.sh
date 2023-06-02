2aaf139ecd8fa04cee4044d785eefe86da5d86b9
jlzsss <jlz19821031@gmail.com>
2023-06-02 08:43:01 +0800

    修复编译错误

--- diy-part3.sh
+++ diy-part3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #============================================================
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
+# File name: diy-part3.sh
+# Description: OpenWrt DIY script part 3 (After Update feeds)
 # Lisence: MIT
 # Author: P3TERX
 # Blog: https://p3terx.com
